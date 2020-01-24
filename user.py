ed2069a14610599b4eb3c29a1147fc2088767d1d
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-24 17:39:00 -0300

    create a Sqlite3 database for users authentication

--- user.py
+++ user.py
@@ -1,5 +1,44 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+    
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor() # collecting data from here
+
+        query = "SELECT * FROM users WHERE username=?" # search user
+        result = cursor.execute(query, (username,))  # apply a tuple to the query
+        row = result.fetchone()  # return the first row found
+
+        if row is not None:
+            user = cls(*row) # parse a set of positional arguments
+        else:
+            user = None  # user not found
+
+        connection.close()
+        return user
+
+
+    @classmethod
+    def find_by_user_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor() 
+
+        query = "SELECT * FROM users WHERE id=?" # search id
+        result = cursor.execute(query, (_id, ))  
+        row = result.fetchone() 
+
+        if row is not None:
+            user = cls(*row)
+        else:
+            user = None  
+
+        connection.close()
+        return user
