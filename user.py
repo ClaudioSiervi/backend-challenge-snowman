3ea5fd2c027f1602b3e337b2bdf68757d16ca681
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-24 20:37:43 -0300

    add user register

--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from flask_restful import Resource, reqparse
+
 
 class User:
     def __init__(self, _id, username, password):
@@ -42,3 +44,36 @@ class User:
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                type=str,
+                required=True,
+                help='This field cannot be black.'
+    )
+    parser.add_argument('password',
+                type=str,
+                required=True,
+                help='This field cannot be blank.'
+    )
+
+    
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']) is not None:
+            return {"messege ": "This username already exists."}
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO users VALUES (NULL, ?, ?)'
+        cursor.execute(query, (data['username'], data['password'],))
+
+        connection.commit()
+        connection.close()
+
+        return ({"messege ": "User created."})
