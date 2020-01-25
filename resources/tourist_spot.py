e23ea5916a10bd65aac4c5810519212d6cd8f138
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-25 03:31:15 -0300

    saving tourist spot into db

--- resources/tourist_spot.py
+++ resources/tourist_spot.py
@@ -1,14 +1,56 @@
 from flask_restful import Resource, reqparse
+import sqlite3
+
+from models.tourist_spot import TrouristSpot
 
 tourist_spots = []
 
-class TouristSpot(Resource):
-   
+class TouristSpotRegister(Resource):
+    
     parser = reqparse.RequestParser()
-    parser.add_argument('gps', type=dict, help='This field cannot be left blank!')
-    parser.add_argument('category', type=str, required=True, help='This field cannot be left blank!')
-    parser.add_argument('pictures', type=dict, help='This field cannot be left blank!')
 
+    parser.add_argument('gps', 
+                type=str, 
+                help='This field cannot be left blank!')
+    parser.add_argument('id_category', 
+                type=str, 
+                required=False, 
+                help='This field cannot be left blank!')
+    parser.add_argument('id_user', 
+                type=str, 
+                help='This field cannot be left blank!')
+
+ # POST /tourist-spot {name:}
+    def post(cls, name):
+
+        request_data = cls.parser.parse_args()  
+        
+        # if TrouristSpot.find_by_name(name) is not None:
+        #     return {"messege ": "This tourist spot already registred."}
+
+        new_spot = {
+                "name": name,
+                "gps": request_data['gps'],
+                "id_category": request_data['id_category'],
+                "id_user": request_data['id_user'] 
+                }
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO tourist_spots VALUES (NULL, ?, ?, ?, ?)'
+        cursor.execute(query, (new_spot['name'], new_spot['gps'], new_spot['id_category'], new_spot['id_user']))
+
+        connection.commit()
+        connection.close()
+
+        return new_spot, 201
+        
+        #tourist_spots.append(new_tourist_spot)
+        #return {"new_tourist_spot": new_tourist_spot}
+
+
+
+class TouristSpot(Resource):
     # GET /tourist-spot/<string:name>
     def get(self, name):
         for tourist_spot in tourist_spots:
@@ -16,19 +58,7 @@ class TouristSpot(Resource):
                 return {"tourist_spot": tourist_spot}
         return {'messege': "tourist spot not found"}
     
-    # POST /tourist-spot {name:}
-    def post(self, name):
-        
-        request_data = TouristSpot.parser.parse_args()  # prevent parsing errors
-        new_tourist_spot = {
-                "name": name,
-                "gps": request_data['gps'],
-                "category": request_data['category'],
-                "pictures": request_data['pictures'] 
-                }
-        tourist_spots.append(new_tourist_spot)   
-        return {"new_tourist_spot": new_tourist_spot}
-
+   
 
 class TouristSpotList(Resource):
     # GET /tourist-spots
