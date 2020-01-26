a98232d28b7479ab50622f9e01d6c4333ec41d9e
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-26 15:31:22 -0300

    refactory tourist_spot resource and model to the SQLAlchemy class format

--- app.py
+++ app.py
@@ -2,24 +2,37 @@ from flask import Flask, request
 from flask_jwt import JWT, jwt_required
 from flask_restful import Api, Resource, reqparse
 
-from resources.tourist_spot import TouristSpot, TouristSpotList
+from resources.tourist_spot import TouristSpot, TouristSpotList, TouristSpotRegister
 from resources.category import Category, CategoryList
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, UserList
+from db import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.secret_key = "snowman"
+
 api = Api(app)
 
-
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # create a new endpoint /auth
 
 api.add_resource(Category, "/category/<string:name>")
 api.add_resource(CategoryList, "/category")
 api.add_resource(TouristSpot, "/tourist-spot/<string:name>")
+api.add_resource(TouristSpotRegister, "/tourist-spot/register")
 api.add_resource(TouristSpotList, "/tourist-spot")
 api.add_resource(UserRegister, "/register")
+api.add_resource(UserList, "/users")
 
+# prevent to run the app when import this source
+if __name__ == "__main__":
 
-app.run(debug=True, port=4999)
+    db.init_app(app)
+    app.run(debug=True, port=4999)
--- resources/tourist_spot.py
+++ resources/tourist_spot.py
@@ -1,86 +1,54 @@
 from flask_restful import Resource, reqparse
 import sqlite3
 
-from models.tourist_spot import TrouristSpot
+from models.tourist_spot import TouristSpotModel
 
 
 class TouristSpotRegister(Resource):
     
     parser = reqparse.RequestParser()
-
+    
+    parser.add_argument('name', 
+                type=str, 
+                required=True,
+                help='This field cannot be left blank!')
     parser.add_argument('gps', 
                 type=str, 
+                required=False,
                 help='This field cannot be left blank!')
     parser.add_argument('id_category', 
                 type=str, 
                 required=True, 
                 help='This field cannot be left blank!')
-    parser.add_argument('id_user', 
-                type=str, 
-                help='This field cannot be left blank!')
 
- # POST /tourist-spot {name:}
-    def post(self, name):
+ # POST /tourist-spot
+    def post(cls):
 
-        request_data = TouristSpotRegister.parser.parse_args()  
+        data = cls.parser.parse_args()  
 
-        new_spot = {
-                "name": name,
-                "gps": request_data['gps'],
-                "id_category": request_data['id_category'],
-                "id_user": request_data['id_user'] 
-        }
+        tourist_spot = TouristSpotModel(**data) # parse by position
 
         try:
-            TrouristSpot.insert(new_spot)
+            tourist_spot.save_to_db()
         except:
-            return {"Messege": "An error occured inserting the tourist spot."}, 500
-        return new_spot, 201
+             return {"Messege": "An error occured inserting the tourist spot."}, 500
+        return tourist_spot.json(), 201
 
 
 
 # select a specific tourist spot
 class TouristSpot(Resource):
     # GET /tourist-spot/<string:name>
-    def get(self, name):
+    def get(cls, name):
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT name FROM tourist_spots'
-        cursor.execute(query)
-        tourist_spots = cursor.fetchall()
-        
-        connection.commit()
-        connection.close()
-
-        for tourist_spot in tourist_spots:
-            if tourist_spot[0] == name:
-                return {"tourist_spot": tourist_spot[0]}
-
-        return {'messege': "tourist spot not found"} 
+        try:
+            tourist_spot = TouristSpotModel.find_spots_by_name(name)
+        except: 
+            return {'Messege': "Tourist spot not found."} 
+        return {"tourist_spot_name": list(map(lambda x: x.json(), tourist_spot))}
    
 
 class TouristSpotList(Resource):
     # GET /tourist-spots
     def get(self):
-        
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM tourist_spots'
-        result = cursor.execute(query)
-        
-        tourist_spot_list = []
-        for row in result:
-            tourist_spot_list.append({ "id": row[0],
-                            "name": row[1],
-                            "gps": row[2],
-                            "id_category": row[3],
-                            "id_user": row[4] 
-            })
-
-        connection.commit()
-        connection.close()
-
-        return {"tourist_spot_list": tourist_spot_list}
+        return {"tourist_spot_list": list(map(lambda x: x.json(), TouristSpotModel.query.all()))}
