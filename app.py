33af7e1a7026c2eb85dfdfad78d9c2bdf8e4b0a9
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-27 02:20:52 -0300

    fixed syntax error for run server

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from db import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "snowman"
