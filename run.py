2579efedb77871a8b1b388c455b89153e2b00d47
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-27 04:24:47 -0300

    adding categories names when create db

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
 from db import db
+from resources.category import CategoryModel
 
 db.init_app(app)
 
@@ -7,3 +8,14 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+    me = CategoryModel('Park')
+    db.session.add(me)
+    me = CategoryModel('Museum')
+    db.session.add(me)
+    me = CategoryModel('Theater')
+    db.session.add(me)
+    me = CategoryModel('Monument')
+    #"Park", "Museum", "Theater", "Monument" 
+    db.session.add(me)
+    db.session.commit()
