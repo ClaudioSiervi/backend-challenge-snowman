11aa783fa79cd6c5eaa078d88919c2bef4a28663
Cláudio-Siervi <claudio.siervi@gmail.com>
2020-01-29 01:04:00 -0300

    fixing register of categories into new db's

--- app.py
+++ app.py
@@ -27,20 +27,27 @@ def create_tables():
 
     db.create_all()
 
-    # id_user = 1 --> admin
-    us = UserModel(username='claudio', password='snow')
-    db.session.add(us)
+    # add required categories into new db's
+    try:
+        # verify whether there is a registered category
+        category = CategoryModel.find_category_by_id(_id=1)
+        print(category.id)
+    except:
+        # id_user = 1 --> admin
+        us = UserModel(username='claudio', password='snowlabs')
+        db.session.add(us)
 
-    me = CategoryModel('Park', id_user=1)
-    db.session.add(me)
-    me = CategoryModel('Museum', id_user=1)
-    db.session.add(me)
-    me = CategoryModel('Theater', id_user=1)
-    db.session.add(me)
-    me = CategoryModel('Monument', id_user=1)
+        me = CategoryModel('Park', id_user=1)
+        db.session.add(me)
+        me = CategoryModel('Museum', id_user=1)
+        db.session.add(me)
+        me = CategoryModel('Theater', id_user=1)
+        db.session.add(me)
+        me = CategoryModel('Monument', id_user=1)
+        db.session.add(me)
 
-    db.session.add(me)
-    db.session.commit()
+        db.session.commit()
+
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
